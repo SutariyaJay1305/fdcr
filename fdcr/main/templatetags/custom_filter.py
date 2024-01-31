8bdf1daba7802110878a9d8a653f6356c44247cd
mahanttechsolution <112825700+mahanttechsolution@users.noreply.github.com>
2024-01-31 03:04:35 -0500

    last

---
+++ fdcr/main/templatetags/custom_filter.py
@@ -0,0 +1,15 @@
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter() 
+def para(description):
+    text = ''
+    for i in description.split('\n'):
+        text += ('<p class="description">' + i + '</p>')
+    return mark_safe(text)
+
+
+
+register.filter('para', para)
