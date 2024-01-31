4346745d496c48c671264b705fd3fb529a70ef81
mahanttechsolution <112825700+mahanttechsolution@users.noreply.github.com>
2024-01-31 03:52:29 -0500

    last

--- fdcr/main/templatetags/custom_filter.py
+++ fdcr/main/templatetags/custom_filter.py
@@ -6,10 +6,29 @@ register = template.Library()
 @register.filter() 
 def para(description):
     text = ''
+    is_new_line = False
+    descriptions = description.split('\n')
+    for i in descriptions:
+        if is_new_line:
+            text += ('<p class="txt-sec2 pb-5">' + i + '</p>')
+    
+        else:
+            if (len(descriptions)) == 1:
+                text += ('<p class="txt-sec2 pt-lg-3 pb-5 pt-5">' + i + '</p>')
+            else:
+                text += ('<p class="txt-sec2 pt-lg-3 pt-5">' + i + '</p>')
+            is_new_line = True
+
+    return mark_safe(text)
+
+@register.filter() 
+def para_2(description):
+    text = ''
     for i in description.split('\n'):
-        text += ('<p class="description">' + i + '</p>')
+        text += ('<p>' + i + '</p>')
     return mark_safe(text)
 
 
 
 register.filter('para', para)
+register.filter('para_2', para_2)
