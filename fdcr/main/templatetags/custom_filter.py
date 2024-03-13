26e7f430c4e92e68f8376def7b3504918120c896
mahanttechsolution <112825700+mahanttechsolution@users.noreply.github.com>
2024-03-13 00:06:11 -0400

    Update custom_filter.py

--- fdcr/main/templatetags/custom_filter.py
+++ fdcr/main/templatetags/custom_filter.py
@@ -10,7 +10,7 @@ def para(description):
     descriptions = description.split('\n')
     for i in descriptions:
         if is_new_line:
-            text += ('<p class="txt-sec2 pb-5">' + i + '</p>')
+            text += ('<p class="txt-sec2 ">' + i + '</p>')
     
         else:
             if (len(descriptions)) == 1:
@@ -18,7 +18,7 @@ def para(description):
             else:
                 text += ('<p class="txt-sec2 pt-lg-3 pt-5">' + i + '</p>')
             is_new_line = True
-
+    print(text)
     return mark_safe(text)
 
 @register.filter() 
